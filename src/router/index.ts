bfba4ae1e85051b95d2a08703e6c705fcad214ed
mobina <mobinazarein049@google.com>
2025-06-10 09:10:57 +0330

    .

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
 import { RouteRecordRaw } from "vue-router";
 import HomePage from "../views/HomePage.vue";
-import LoginPage from "@/views/LoginPage.vue";
+import LoginPage from "../views/loginPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
